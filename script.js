e0c288594aaaedcc967238c6712fa5d896ec8408
Amit Purohit <amitpurohit.dev@gmail.com>
2021-12-21 17:03:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('nav .nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((links, index) => {
+        if (links.style.animation) { links.style.animation = '' }
+        else {
+            links.style.animation = `navLinksFade 0.5s ease forwards ${index / 5}s`
+        }
+    });
+    burger.classList.toggle('toggle');
+    });
+}
+navSlide();
